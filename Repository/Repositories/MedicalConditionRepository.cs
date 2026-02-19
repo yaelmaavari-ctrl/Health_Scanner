b38edd38d7d47a825a6cada9233c89dcb72cfe39
Yael <yaelmaavari@gmail.com>
2026-02-19 22:23:43 +0200

    add repositories

---
+++ Repository/Repositories/MedicalConditionRepository.cs
@@ -0,0 +1,46 @@
+﻿using Repository.Entities;
+using Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repositories
+{
+    public class MedicalConditionRepository : IRepository<MedicalCondition>
+    {
+        private readonly Icontext _context;
+        public MedicalConditionRepository(Icontext context) { 
+            _context = context;
+        }
+
+        public async Task<MedicalCondition> AddItem(MedicalCondition item)
+        {
+            if(item == null)
+                return null;
+            await _context.MedicalConditions.AddAsync(item);
+            await _context.SaveChangesAsync();
+        }
+
+        public Task DeleteItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<MedicalCondition>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MedicalCondition> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MedicalCondition> UpdateItem(int id, MedicalCondition item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Repository/Repositories/UserConditionRepository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repository.Entities;
+using Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repositories
+{
+    public class UserConditionRepository:IRepository<UserCondition>
+    {
+        private readonly Icontext _context;
+        public UserConditionRepository(Icontext context)
+        {
+            _context = context;
+        }
+
+        public async Task<UserCondition> AddItem(UserCondition item)
+        {
+            if (item ==null)
+            {
+                return null;
+            }
+            await _context.UserConditions.AddAsync(item);
+            await _context.Save();
+            return item;
+        }
+
+        public async Task DeleteItem(int id)
+        {
+           var uc = _context.UserConditions.FirstOrDefault(u => u.Id == id);
+            if (uc != null)
+            {
+                _context.UserConditions.Remove(uc);
+                await _context.Save();
+            }
+        }
+
+        public Task<List<UserCondition>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserCondition> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserCondition> UpdateItem(int id, UserCondition item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
